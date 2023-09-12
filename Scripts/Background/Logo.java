2cd913dc00646d61cb6c6157622925e774462673
TheVaxStudio <rodrigomassanori@hotmail.com>
2023-09-12 14:51:36 -0300

    all

--- Scripts/Background/Logo.java
+++ Scripts/Background/Logo.java
@@ -1,6 +1,14 @@
 package Background;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Logo
 {
-
+	public Logo() throws IOException
+	{
+		BufferedImage LogoScreen = ImageIO.read(new File("./GameLogo/LogoVaxomon.pmg"));
+	}
 }
