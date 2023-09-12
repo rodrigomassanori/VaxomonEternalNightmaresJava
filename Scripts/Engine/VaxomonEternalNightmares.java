80530b0d33cfa964410d699cf8ba02cf75febba3
TheVaxStudio <rodrigomassanori@hotmail.com>
2023-09-12 08:17:52 -0300

    all

---
+++ Scripts/Engine/VaxomonEternalNightmares.java
@@ -0,0 +1,36 @@
+package Engine;
+
+import window.Window;
+
+public class VaxomonEternalNightmares implements IGameLogic
+{
+    @Override
+    public void Init(Window window) throws Exception 
+    {
+        throw new UnsupportedOperationException("Unimplemented method 'Init'");
+    }
+
+    @Override
+    public void Input(Window window) 
+    {
+        throw new UnsupportedOperationException("Unimplemented method 'Input'");
+    }
+
+    @Override
+    public void Update(float Interval) 
+    {
+        throw new UnsupportedOperationException("Unimplemented method 'Update'");
+    }
+
+    @Override
+    public void Render(Window window) 
+    {
+        throw new UnsupportedOperationException("Unimplemented method 'Render'");
+    }
+
+    @Override
+    public void CleanUp() 
+    {
+        throw new UnsupportedOperationException("Unimplemented method 'CleanUp'");
+    }
+}
--- Scripts/Game.java
+++ Scripts/Game.java
@@ -1,15 +1,27 @@
+import Engine.GameEngine;
+import Engine.IGameLogic;
+import Engine.VaxomonEternalNightmares;
+
 public class Game 
 {
     public static void main(String[] args) 
     {
         try
         {
+            boolean Sync = true;
 
+            IGameLogic gameLogic = new VaxomonEternalNightmares();
+
+            GameEngine gameEngine = new GameEngine("Vaxomon Eternal Nightmares", 1440, 960, Sync, gameLogic);
+
+            gameEngine.run();
         }
 
-        catch(Exception e)
+        catch (Exception E)
         {
-            
+            E.printStackTrace();
+
+            System.exit(-1);
         }
     }
 }
